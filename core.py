c6571099784c149b415ed5fed3a681d6c71611bc
liu946 <liuyang570926881@gmail.com>
2015-03-06 22:38:36 +0800

    列表支持

--- core.py
+++ core.py
@@ -13,9 +13,26 @@ class IO(object):
 		super(IO, self).__init__()
 		self.config=json.load(open(configfile))
 		self.iomap=self.config["iomap"]
+		self.pinnum=len(self.iomap)
 		self.mode()
 
 	def mode(m=GPIO.BOARD):
 		GPIO.setmode(m)
+
+	def setup(pin,mode):
+		if isinstance(pin,int):
+			pin=[pin]
+		for i in pin:
+			GPIO.setup(self.iomap[i],mode)
+
+	def set(l,o):
+		if isinstance(l,int):
+			l=[l]
+		for i in l:
+			GPIO.output(self.iomap[i],o)
+
+	def get(pin):
+		return GPIO.input(self.iomap[pin])
+
 
 
